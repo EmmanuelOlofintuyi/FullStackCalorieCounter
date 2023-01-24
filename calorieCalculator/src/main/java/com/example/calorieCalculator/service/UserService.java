121d4befedd4269cb51c29f7aa4815454cef21bd
emmanuelolofintuyi <EmmanuelOlofintuyi@gmail.com>
2023-01-24 14:02:07 -0500

    created calorieCounter database and updated Service to save and get users

--- calorieCalculator/src/main/java/com/example/calorieCalculator/service/UserService.java
+++ calorieCalculator/src/main/java/com/example/calorieCalculator/service/UserService.java
@@ -1,4 +1,11 @@
 package com.example.calorieCalculator.service;
 
+import com.example.calorieCalculator.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    public User saveUser(User user);
+
+    public List<User> getAllUsers();
 }
--- calorieCalculator/src/main/java/com/example/calorieCalculator/service/UserServiceImpl.java
+++ calorieCalculator/src/main/java/com/example/calorieCalculator/service/UserServiceImpl.java
@@ -1,4 +1,24 @@
 package com.example.calorieCalculator.service;
 
-public class UserServiceImpl {
+import com.example.calorieCalculator.model.User;
+import com.example.calorieCalculator.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
