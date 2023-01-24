3cefbc1db289219243dd63239efe2b5258bef7ae
emmanuelolofintuyi <EmmanuelOlofintuyi@gmail.com>
2023-01-24 14:14:17 -0500

    updated the controller, so I can get users from database and post users in the database

--- calorieCalculator/src/main/java/com/example/calorieCalculator/controller/UserController.java
+++ calorieCalculator/src/main/java/com/example/calorieCalculator/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.example.calorieCalculator.controller;
 
+import com.example.calorieCalculator.model.User;
+import com.example.calorieCalculator.service.UserService;
+import com.example.calorieCalculator.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/calorie_counter")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/addUser")
+    public String add(@RequestBody User user){
+        userService.saveUser(user);
+        return "New user added to the database";
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+
 }
