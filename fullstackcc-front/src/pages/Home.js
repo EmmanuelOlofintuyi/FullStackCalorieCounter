f0a14e2cb9882fa87db7abc4fad5e6c68c6348a2
emmanuelolofintuyi <EmmanuelOlofintuyi@gmail.com>
2023-01-27 11:25:05 -0500

    - added Home page to frontend - has all 4 buttons and navbar - closes #6

---
+++ fullstackcc-front/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='container'>
+        <div className='py-4'>
+        <table className="table">
+  <thead>
+    <tr>
+      <td scope="col">Actions</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td ><button className='btn btn-primary mx-2'>View</button></td>
+    </tr>
+    <tr>
+      <td ><button className='btn btn-outline-primary mx-2'>Edit</button></td>
+    </tr>
+    <tr>
+      <td ><button className='btn btn-danger mx-2'>View</button></td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
+  )
+}
