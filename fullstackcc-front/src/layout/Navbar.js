bc271fb7bd42c60f022015eecab9132a7261ec67
emmanuelolofintuyi <EmmanuelOlofintuyi@gmail.com>
2023-01-30 21:55:56 -0500

    Links *Calories Counter* label to Home page

--- fullstackcc-front/src/layout/Navbar.js
+++ fullstackcc-front/src/layout/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Calorie Counter</a>
+      <Link className="navbar-brand" to={'/'}>Calorie Counter</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
